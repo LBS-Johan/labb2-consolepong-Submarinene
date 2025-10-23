4066b5b6f12945b252447dbec25fb66cc8cf0be6
Submarinene <submarinenene@gmail.com>
2025-10-23 14:41:55 +0200

    lektion 1 typ

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata;
@@ -19,29 +20,42 @@ namespace Labb2_ConsolePong
             height = Console.WindowHeight;
             Console.CursorVisible = false;
 
-
+            
         }
+
+        public Paddle p1 = new Paddle();
+        public Paddle p2 = new Paddle();
+
+
 
         public bool Run()
         {
             //Töm hela skärmen i början av varje uppdatering.
             Console.Clear();
 
+            p1.Draw(110,5);
+            p2.Draw(10,5); 
+
             if (Input.IsPressed(ConsoleKey.UpArrow))
             {
-                //Flytta spelare 1 uppåt
+                p1.Move(-1);  //Flytta spelare 1 uppåt
             }
             if (Input.IsPressed(ConsoleKey.DownArrow))
             {
+                p1.Move(+1);  
                 //Flytta spelare 1 nedåt
             }
 
             if (Input.IsPressed(ConsoleKey.W))
             {
+
+                p2.Move(-1);
                 //Flytta spelare 2 uppåt
             }
             if (Input.IsPressed(ConsoleKey.S))
             {
+                p2.Move(+1);
+               
                 //Flytta spelare 2 nedåt
             }
 
---
+++ Paddle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_ConsolePong
+{
+    internal class Paddle
+    {
+       public int x = 5;
+       public int y = 5;
+       public int size = 5;
+
+        public void Move(int yAmount)
+        {
+            y += yAmount;
+        }
+
+        public void Draw(int x, int y)
+        {
+                 int i = 1;
+                Console.SetCursorPosition(x, this.y + i );
+                Console.Write("|");
+           
+        }
+
+
+        /*  public Paddle(int x, int y, int size)
+          {
+              this.x = x;
+              this.y = y;
+              this.size = size;
+          }
+
+          */
+
+
+
+
+
+
+        /*
+         x av typen int som representerar spelarens x-position.
+         y av typen int som representerar spelarens y-position.
+         size av typen int som representerar spelarens storlek.
+        
+         
+         */
+    }
+}
